8501be14d7f6f1e50e78f7acab96cffdefc8ce16
Dryzin <endrioemanuel11@gmail.com>
2023-02-27 19:58:23 -0300

    mudando o banco de dados

--- db/dummy_db.sql
+++ db/dummy_db.sql
@@ -11,72 +11,63 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+#criação de uma tabela
+CREATE TABLE usuario (
+id INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(255),
+cpf VARCHAR(255),
+tipo ENUM('docente', 'administrador')
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE docentes (
+ra INT PRIMARY KEY,
+usuario_id INT unique,
+FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+);
 
---
--- Database: `dummy_db`
---
+CREATE TABLE turma (
+id INT PRIMARY KEY,
+nome VARCHAR(255),
+tipo ENUM('trilhas', 'aprendizagem'),
+carga_horaria Time
+);
 
--- --------------------------------------------------------
+CREATE TABLE uc (
+id INT AUTO_INCREMENT PRIMARY KEY,
+nome_uc varchar(255),
+num_turma int,
+carga_horaria Time,
+FOREIGN KEY (num_turma) REFERENCES turma (id)
+);
 
---
--- Table structure for table `schedule_list`
---
-
-CREATE TABLE `schedule_list` (
-  `id` int(30) NOT NULL,
-  `title` text NOT NULL,
-  `description` text NOT NULL,
-  `start_datetime` datetime NOT NULL,
-  `end_datetime` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `schedule_list`
---
-
+#tabela criada através de uma relaçção N:M
 CREATE TABLE calendario_de_aula (
 id INT AUTO_INCREMENT PRIMARY KEY,
 ra_docente INT,
 id_uc INT,
-horario_inicio DATETIME NOT NULL,
-horario_fim DATETIME DEFAULT NULL
+horario_inicio DATETIME,
+horario_fim DATETIME,
+UNIQUE (ra_docente, horario_inicio, horario_fim),
+UNIQUE (id_uc, horario_inicio, horario_fim),
+FOREIGN KEY (ra_docente) REFERENCES docentes (ra),
+FOREIGN KEY (id_uc) REFERENCES uc (id)
 );
 
+#INSERTS
+INSERT INTO usuario (nome, cpf, tipo)
+VALUES ('João da Silva', '111.111.111-11', 'administrador'), ('Matheus', '222.222.222-22', 'docente'), ('Cadu', '333.333.333-33', 'docente');
 
-INSERT INTO `schedule_list` (`id`, `title`, `description`, `start_datetime`, `end_datetime`) VALUES
-(1, 'Sample 101', 'This is a sample schedule only.', '2023-02-10 10:30:00', '2023-02-10 18:00:00'),
-(2, 'Sample 102', 'Sample Description', '2023-02-12 14:00:00', '2023-02-12 17:00:00');
+INSERT INTO docentes (ra, usuario_id)
+VALUES (1002, 2), (1003, 3);
+
+INSERT INTO turma (id, nome, tipo, carga_horaria)
+VALUES
+  ('0222','sistema','trilhas','820:00'),('0333','redes','trilhas','800:00'),('0444','administração','aprendizagem','780:00');
+  
+INSERT INTO uc (nome_uc, num_turma, carga_horaria)
+VALUES ('web', 222, '50:00:00'),('desktop', 333, '40:00:00'), ('mobile', 222, '10:00:00'),('hardware', 333, '32:00:00'), ('Arquivos digitais', 444, '12:00:00') ;
+
 
 INSERT INTO `calendario_de_aula` (`ra_docente`, `id_uc`, `horario_inicio`, `horario_fim`) VALUES
-(1003, 1, '2023-02-19 10:30:00', '2023-02-19 18:00:00'),
-(2002, 2, '2023-02-20 14:00:00', '2023-02-20 17:00:00');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `schedule_list`
---
-ALTER TABLE `schedule_list`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `schedule_list`
---
-ALTER TABLE `schedule_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+(1002, 1, '2023-02-19 07:30:00', '2023-02-19 11:30:00'),
+(1003, 2, '2023-02-20 13:30:00', '2023-02-20 17:30:00');
